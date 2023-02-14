2162231ca93d643744305507d2838a97dce32b31
Telini07 <brunotelinirocha50@gmail.com>
2023-02-14 12:11:37 -0300

    Exemplos aula 1

---
+++ aula1/exemplo.php
@@ -0,0 +1,13 @@
+<?php
+    $ola = "Olá mundo";
+    echo gettype($ola);
+    echo "<br>";
+    echo $ola;
+    //Daqui para frente
+    //variavel olá e inteira
+    $ola = 10;
+    echo "<br>";
+    echo gettype($ola);
+    echo "<br>";
+    echo $ola;
+?>
---
+++ aula1/exemplo5.php
@@ -0,0 +1,9 @@
+<?php
+    $nome = "Bruno";
+    $sobrenome = "Telini Rocha";
+
+    echo $nome." ".$sobrenome."<br>";
+    echo "<br>";
+    echo "$nome $sobrenome";
+
+?>
